d28f9ab14ae3d9dbe28f252708b8c7a89e76e0c3
jotas1000000@hotmail.com <jotas1000000@hotmail.com>
2020-04-03 01:33:01 -0400

    Primer commit/Primeras configuraciones/Backend & Frontend

---
+++ Backend/PequeInnovaAPI/PequeInnovaAPI/Data/APIProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PequeInnovaAPI.Data
+{
+    public class APIProfile:Profile
+    {
+        public APIProfile()
+        {
+
+        }
+    }
+}
