2f27b4ccca1e254a0302996f1ac96bb9679f3391
NITRO <oveliskooo@hotmail.com>
2020-04-12 14:43:50 -0600

    arreglado lo de las rutas-

--- Backend/PequeInnovaAPI/PequeInnovaAPI/Exceptions/BadOperationRequest.cs
+++ Backend/PequeInnovaAPI/PequeInnovaAPI/Exceptions/BadOperationRequest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RestaurantAPI.Exceptions
+namespace PequeInnovaAPI.Exceptions
 {
     public class BadOperationRequest: Exception
     {
